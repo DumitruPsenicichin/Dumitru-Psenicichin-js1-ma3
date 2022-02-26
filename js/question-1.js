2b606b67a251d60b64d21ec7589f0046e4c69800
Dumitru <nrj2asus@gmail.com>
2022-02-26 23:41:45 +0100

    main

--- js/question-1.js
+++ js/question-1.js
@@ -2,4 +2,14 @@
  
 const getRemainder = (a, b) => {
   return a % b;
+};
+
+const CORS_URL = "https://noroffcors.herokuapp.com/";
+const API_URL = "https://elephant-api.herokuapp.com/elephants";
+
+const corsFixedUrl = CORS_URL + API_URL;
+async function getElephants() {
+  const response = await fetch(corsFixedUrl);
+  const results = await response.json();
+  console.log(results);
 }
--- js/question-2.js
+++ js/question-2.js
@@ -1,29 +1,52 @@
+const url = 'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=85e9085d454a45e393e73feec5dd57c5';
 
-
-const url = 'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=6bd67867840845f3b159a53e2cd789b5';
-const gameList = document.querySelector('.game_list');
+const gameList = document.querySelector('.load_list');
 
 
 async function displayGameList() {
-  try {
-      const response = await fetch(url);
+  const response = await fetch(url);
 
-      const data = await response.json();
+  const games = await response.json();
 
-      console.log(data);
+  result = games.results;
 
-      for (let i = 0; i < data.length; i++) {
+  console.log(games);
 
-        if (i === 8) {
-            break;
-        }
+  gameList.innerHTML = '';
 
-        gameList.innerHTML += `<div class="game">${data[i].name}</div>`;
+  for (i = 0; i < result.length; i++) {
+    console.log(result[i].name);
+
+    if (i === 8) {
+      break;
     }
 
-  } catch (error) {
-      gameList.innerHTML = "Something went wrong during getting API";
+    gameList.innerHTML += `<div class = game_item>
+        ${i + 1}: ${result[i].name}<br>  
+        Rating: ${result[i].rating}<br>
+        Number of tags: ${result[i].tags.length}</div><br>`
   }
-} 
+}
 
 displayGameList();
+
+/* Make a call to the Rawg API.
+
+Go to https://rawg.io/apidocs and get an API key which you’ll use as part of the endpoint you’re making an API call to. You can use https://noroff.no for the URL and Noroff Assignment for the description.
+
+You'll be given an API Key you can add as a "key" parameter in your fetch request.
+
+Make a call to the following API endpoint replacing INSERTAPIKEYHERE with the key given to you from the Rawg API.
+https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=INSERTAPIKEYHERE
+
+Loop through the results and display the following properties in HTML, but only for the first eight results:
+name
+rating
+number of tags (not the tag details, just the amount of tags)
+The styling for this assignment is not important but loading indicator should be displayed while the API call is in progress.
+
+Be sure to handle any potential errors in the code.
+
+You can use either regular promise or async/await syntax to make the call.
+
+Be sure to arrange all file types appropriately, consult the repos from the lessons for examples. */
