c74bf0d84d22d5ee3a8113f00c909999b27a7a50
Dumitru <nrj2asus@gmail.com>
2022-02-27 10:37:09 +0100

    main

--- js/question-2.js
+++ js/question-2.js
@@ -1,37 +1,34 @@
-const url = 'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=85e9085d454a45e393e73feec5dd57c5';
+const url =
+	"https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=85e9085d454a45e393e73feec5dd57c5";
 
-const gameList = document.querySelector('.game_list');
+const gameList = document.querySelector(".game_list");
 
+async function displayGameList() {
+	try {
+		const response = await fetch(url);
+		const games = await response.json();
 
- async function displayGameList() {
-   try{
-  const response = await fetch(url);
-  const games = await response.json();
+		result = games.results;
+		console.log(games);
 
-  result = games.results;
+		gameList.innerHTML = "";
 
-  console.log(games);
+		for (i = 0; i < result.length; i++) {
+			console.log(result[i].name);
 
-  gameList.innerHTML = '';
+			if (i === 8) {
+				break;
+			}
 
-  for (i = 0; i < result.length; i++) {
-    console.log(result[i].name);
-
-    if (i === 8) {
-      break;
-    }
-
-    gameList.innerHTML += `<div class = game_item>
+			gameList.innerHTML += `<div class = game_item>
         ${i + 1}: ${result[i].name}<br>  
         Rating: ${result[i].rating}<br>
         Number of tags: ${result[i].tags.length}</div><br>`;
-  }
- }catch{
-   console.log('Something went wrong...');
-   gameList.innerHTML = 'Something went wrong...';
- }
+		}
+	} catch {
+		console.log("Something went wrong...");
+		gameList.innerHTML = "Something went wrong...";
+	}
 }
 
 displayGameList();
-
-  
